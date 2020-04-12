69287c84790900dfda781e9bd88b9c0372dfc692
yayanrachmad <yayanshare@gmail.com>
2020-04-12 18:32:15 +0700

    loginActivity

--- app/src/main/java/com/android/second/MainActivity.java
+++ app/src/main/java/com/android/second/MainActivity.java
@@ -21,16 +21,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         lstWeapon = new ArrayList<>();
-        lstWeapon.add(new weapon("AK-47","deskripsi",R.drawable.akempattuju));
-        lstWeapon.add(new weapon("Assault Riffle","deskripsi",R.drawable.assaultrifle));
-        lstWeapon.add(new weapon("Beretta M9","deskripsi",R.drawable.beretta));
-        lstWeapon.add(new weapon("Brass Knuckles","deskripsi",R.drawable.brassknuckles));
-        lstWeapon.add(new weapon("M249 Light Machine Gun","deskripsi",R.drawable.mlightmachinegun));
-        lstWeapon.add(new weapon("AK-47","deskripsi",R.drawable.akempattuju));
-        lstWeapon.add(new weapon("Assault Riffle","deskripsi",R.drawable.assaultrifle));
-        lstWeapon.add(new weapon("Beretta M9","deskripsi",R.drawable.beretta));
-        lstWeapon.add(new weapon("Brass Knuckles","deskripsi",R.drawable.brassknuckles));
-        lstWeapon.add(new weapon("M249 Light Machine Gun","deskripsi",R.drawable.mlightmachinegun));
+        lstWeapon.add(new weapon("AK-47","keterangan Senjata :  " +
+                "                                         " +
+                "singkatan dari Avtomat Kalashnikova 1947,adalah senapan serbu yang dirancang oleh Mikhail Kalashnikov, " +
+                "diproduksi oleh pembuat senjata Rusia IZhMASh, dan digunakan oleh banyak negara Blok Timur semasa Perang Dingin. " +
+                "Senapan ini diadopsi dan dijadikan senapan standar Uni Soviet pada tahun 1947.[2] Jika dibandingkan dengan senapan yang digunakan semasa Perang Dunia II, " +
+                "AK-47 mempunyai ukuran lebih kecil, dengan jangkauan yang lebih pendek, " +
+                "memakai peluru dengan kaliber 7,62 x 39 mm yang lebih kecil, dan memiliki pilihan tembakan (selective-fire)." +
+                "AK-47 termasuk salah satu senapan serbu pertama dan hingga kini merupakan senapan serbu yang paling banyak diproduksi.",R.drawable.akempattuju));
+        lstWeapon.add(new weapon("Assault Riffle","keterangan Senjata : " +
+                "                                       " +
+                "Senapan serbu (bahasa Inggris: Assault rifle) adalah senjata api otomatis yang merupakan senapan laras panjang atau karabin, " +
+                "yang memiliki pilihan tembakan (selective-fire), dan menggunakan amunisi kaliber menengah. " +
+                "Senapan serbu masuk dalam kategori di antara senapan mesin ringan, yang berfungsi untuk menembak secara full-otomatis sebagai senjata pendukung, " +
+                "dan submachine gun, senjata otomatis yang menggunakan peluru ukuran peluru pistol sebagai senjata api jarak dekat. " +
+                "Senapan serbu sudah menjadi persenjataan standar untuk tentara modern, menggantikan senapan laras panjang era Perang Dunia II, " +
+                "seperti M1 Garand dan SKS.",R.drawable.assaultrifle));
+        lstWeapon.add(new weapon("Beretta M9","keterangan Senjata : " +
+                "                                      " +
+                "Diterjemahkan dari bahasa Inggris-Beretta M9 — secara resmi Pistol, Semiautomatic, 9mm, " +
+                "M9 — adalah sebutan untuk pistol semi-otomatis Beretta 92F yang digunakan oleh Angkatan Bersenjata Amerika Serikat. " +
+                "M9 diadopsi oleh militer Amerika Serikat sebagai pistol dinas mereka pada tahun 1985.",R.drawable.beretta));
+        lstWeapon.add(new weapon("Brass Knuckles","keterangan Senjata : " +
+                "                                       " +
+                "Keling (bahasa Inggrisː brass knuckles, knuckles, knucks, brass knucks, knucklebusters, knuckledusters, an English punch atau classic) " +
+                "adalah sebuah senjata yang terbuat dari logam yang dapat dipasang melingkari keempat buku jari terdepan dari tangan. " +
+                "Senjata digunakan dalam pertarungan jarak dekat fisik antar individu yang mengutamakan tinju. " +
+                "Jenis logam yang digunakan umumnya yang memiliki tingkat kekerasan dan massa jenis yang tinggi untuk " +
+                "menambah tingkat kerusakan yang diakibatkan oleh senjata tersebut. Pegangan (grip) " +
+                "yang terpasang di dalam kepalan tangan mencegah kerusakan pada buku jari pengguna " +
+                "karena gaya balik yang diakibatkan dari pukulan tersebut (Hukum Newton Ketiga)",R.drawable.brassknuckles));
+        lstWeapon.add(new weapon("M249 Light Machine Gun","keterangan Senjata :     " +
+                "                                       " +
+                "Senapan mesin ringan M249 (Inggris: M249 light machine gun (LMG), sebelumnya bernama M249 Squad Automatic Weapon (SAW) (Senjata Otomatis Regu), " +
+                "secara resmi bernama Light Machine Gun, 5.56 mm, M249 adalah senapan mesin operasi gas berpendingin udara menggunakan " +
+                "amunisi kaliber 5,56 mm varian FN Minimi buatan FN Herstal asal Belgia versi Amerika yang digunakan oleh " +
+                "Angkatan Bersenjata Amerika Serikat. Senapan ini merupakan jawaban atas kebutuhan militer Amerika Serikat akan senjata api " +
+                "yang mempunyai rata-rata tembakan (rate of fire), akurasi dan mobilitas yang tinggi.",R.drawable.mlightmachinegun));
+
 
         RecyclerView myrv = (RecyclerView) findViewById(R.id.recycleview_id);
         RecycleViewAdapter myAdapter = new RecycleViewAdapter(this,lstWeapon);
@@ -40,13 +68,7 @@ public class MainActivity extends AppCompatActivity {
 
         btnExit = (Button) findViewById(R.id.btnExit);
 
-        btnExit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //perintah untuk mengakhiri aplikasi
-                finish();
-            }
-        });
+
 
     }
 }
--- app/src/main/java/com/android/second/Weapon_Activity.java
+++ app/src/main/java/com/android/second/Weapon_Activity.java
@@ -2,13 +2,33 @@ package com.android.second;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Weapon_Activity extends AppCompatActivity {
+
+    private TextView tvnama,tvdeskripsi ;
+    private ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weapon_);
+
+        tvnama = (TextView) findViewById(R.id.txtnama);
+        tvdeskripsi = (TextView) findViewById(R.id.txtdeskripsi);
+        img = (ImageView) findViewById(R.id.thumbnail);
+
+        //recieve data
+        Intent intent = getIntent();
+        String Nama = intent.getExtras().getString("Nama");
+        String deskripsi = intent.getExtras().getString("deskripsi");
+        int image = intent.getExtras().getInt("Thumbnail");
+
+        tvnama.setText(Nama);
+        tvdeskripsi.setText(deskripsi);
+        img.setImageResource(image);
     }
 }
